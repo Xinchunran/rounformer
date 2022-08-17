f31e1d5b06c5f8b834aed725e842f27e2556a5b2
Xinchunran <xinchun@accre.vu>
2022-08-17 18:25:52 -0500

    discriminator

--- model_genrative.py
+++ model_genrative.py
@@ -21,4 +21,27 @@ class Generator16(nn.Module):
             ),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False),
+        )
+
+
+     def forward(self, input):
+         return self.main(input)
+
+
+class Discriminator16(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator16, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.1),
+
 
